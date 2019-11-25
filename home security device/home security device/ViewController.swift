2cb100873f433c53517eaa1e69dfa072dddd436a
samwise2 <sam.orend22@gmail.com>
2019-11-25 16:39:55 -0500

    Initial UI

---
+++ home security device/home security device/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  home security device
+//
+//  Created by Sam Orend on 2019-11-25.
+//  Copyright © 2019 Sam Orend. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var DoorSwitch: UISwitch!
+    @IBOutlet weak var MotionSwitch: UISwitch!
+    @IBOutlet weak var BuzzerSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+/*class dropDownBtn: <#super class#> {
+    <#code#>
+}*/
+
